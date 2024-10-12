f3be0c05575bd8196b10a4121eb49e5bb31be86a
Ranvir Singh <78982134+Thinking-cell@users.noreply.github.com>
2024-10-12 13:10:08 -0400

    added services

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 // constats for scroll animations
 const scrollAnimationObserver = new IntersectionObserver( scrollAnimationCallback, {threshold: 0.2})
 const scrollTargets = document.querySelectorAll(".anim-on-scroll");
+// For service box 
+const boxHeight = 250 //in px
+const boxheightMargin = 55
+let screenFactor = 2
+let shrinkedHeight =  screenFactor*(boxHeight+boxheightMargin) + "px"
+const showMoreBtn = document.getElementById("show-more")
+const services = document.querySelector(".services-grid")
 
 
 
@@ -12,7 +19,22 @@ const scrollTargets = document.querySelectorAll(".anim-on-scroll");
 
 
 
-
+// show more box logic
+services.style.height = shrinkedHeight
+showMoreBtn.addEventListener("click",()=>{
+ 
+  if(showMoreBtn.classList.contains("more")){
+    services.style.height = 100 + "%"
+    showMoreBtn.classList.remove("more")
+    showMoreBtn.classList.add("less")
+    showMoreBtn.textContent = "Show less"
+  }else{
+    services.style.height = shrinkedHeight
+    showMoreBtn.classList.remove("less")
+    showMoreBtn.classList.add("more")
+    showMoreBtn.textContent = "Show more"
+  }
+})
 
 
 
